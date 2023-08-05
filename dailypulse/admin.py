4a2e1935f7dea8223220a6b69e7a99f9440817c4
dinajar <deenajaradat2@gmail.com>
2023-08-05 15:45:57 +0300

    reading later

--- dailypulse/admin.py
+++ dailypulse/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Dailypulse
+from .models import Dailypulse , Reading_later
 
 # Register your models here.
 admin.site.register(Dailypulse)
+admin.site.register(Reading_later)
--- dailypulse/serializers.py
+++ dailypulse/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Dailypulse
+from .models import Dailypulse,Reading_later
 
 
 class DailypulseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dailypulse
+        fields = "__all__"
+
+class ReadingLaterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reading_later
         fields = "__all__"
