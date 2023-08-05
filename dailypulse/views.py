420f69f8f8f125543c7f022c08b6670cb16b035a
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-08-05 19:47:04 +0300

    migrate

--- dailypulse/views.py
+++ dailypulse/views.py
@@ -17,3 +17,26 @@ class DailypulseDetail(RetrieveUpdateDestroyAPIView):
     queryset = Dailypulse.objects.all()
     serializer_class = DailypulseSerializer
 
+
+# create reading list : body attributes  all exepet id
+# delete reading list : requeiring id in url
+# get reading list : requiring user id in url
+
+# create comment : body attributes all
+# delete comment : comment id url
+# get comment : requeering user id and news id in url
+# get comments : requeering news id in url
+# update comment : comment id url ,body description
+
+# create News : body attributes all
+# get news : requering user id in url
+# delete news : requeiring news id
+
+# API ROUTES :
+# route to get data require a category(q)
+# route to get arabic news require a category(q)
+# weather route link 'http://api.openweathermap.org/data/2.5/weather?q=LOCATION&appid=f22892654725839a44ff6db985f0b151' require the user ocation in url
+
+
+# get USER info : user id url
+# update USER : user id url , body attributes all
