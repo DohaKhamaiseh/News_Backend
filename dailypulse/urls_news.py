2aa2e8e41e1f32bf7422fda7c322e3b2c94f3ff8
dinajar <deenajaradat2@gmail.com>
2023-08-06 20:41:55 +0300

    news

--- dailypulse/urls_news.py
+++ dailypulse/urls_news.py
@@ -4,7 +4,7 @@ from .views_news import CreateNewsList,GetNewsList,DeleteNews
 
 urlpatterns = [
     path("Create_News/", CreateNewsList.as_view(), name="Create_News"),
-    path("Delete_News/<int:news_id>/", DeleteNews.as_view(), name="Delete_News"),
+    path("Delete_News/<int:pk>/", DeleteNews.as_view(), name="Delete_News"),
     path("Get_News/<int:user_id>/", GetNewsList.as_view(), name="Get_News"),
 
 ]
