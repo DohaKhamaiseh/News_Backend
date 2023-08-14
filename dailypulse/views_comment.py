5fbe2d8b480cd6013cd54459109242aa73a35527
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-08-14 15:55:39 +0300

    last push hopfully

--- dailypulse/views_comment.py
+++ dailypulse/views_comment.py
@@ -47,7 +47,8 @@ class Get_comment_user(ListAPIView):
     serializer_class = CommentSerializer
     def get_queryset(self):
         user_id = self.kwargs['user_id']
-        return Comment.objects.filter(user_id=user_id )
+        news_id = self.kwargs['news_id']
+        return Comment.objects.filter(user_id=user_id and news_id == news_id)
 
 class Update_comment(UpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly, IsAuthenticated]
